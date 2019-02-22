a321618378899365f970812bc33986a38568300a
Sviatoslav <qqfittoppt@gmail.com>
2019-02-22 13:52:29 +0200

    Implement base classes

---
+++ TicTakLib/Board.cs
@@ -0,0 +1,20 @@
+﻿namespace TicTakLib
+{
+    public class Board
+    {
+        private int width;
+        private int height;
+        private int winCondition;
+        private Cell[,] cells;
+
+        public Board(int width, int height, int winCondition)
+        {
+            this.width = width;
+            this.height = height;
+            this.winCondition = winCondition;
+            cells = new Cell[width, height];
+        }
+
+
+    }
+}
---
+++ TicTakLib/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTakLib
+{
+    public abstract class Player
+    {
+        private TypeOfFigure typeOfFigure;
+        public Player(TypeOfFigure typeOfFigure)
+        {
+            this.typeOfFigure = typeOfFigure;
+        }
+
+        public abstract void MakeTurn(Board board);
+    }
+}
